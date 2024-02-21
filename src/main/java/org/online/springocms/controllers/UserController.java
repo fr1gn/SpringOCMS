12691f4a7d719349ddf0e9879bd012265f96bbce
Tamer <232146@astanait.edu.kz>
2024-02-21 09:50:44 +0600

    tamerlan

--- src/main/java/org/online/springocms/controllers/UserController.java
+++ src/main/java/org/online/springocms/controllers/UserController.java
@@ -3,7 +3,7 @@ import org.online.springocms.models.User;
 import org.online.springocms.repositories.UserRepositoryInterface;
 import org.online.springocms.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//dad
 import java.util.List;
 public class UserController {
 
