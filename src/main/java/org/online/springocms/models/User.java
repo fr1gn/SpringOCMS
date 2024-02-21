3d92367712dad19bfde4831070146c3a00bbbdc3
Tamer <232146@astanait.edu.kz>
2024-02-21 09:25:10 +0600

    tamerlan

--- src/main/java/org/online/springocms/models/User.java
+++ src/main/java/org/online/springocms/models/User.java
@@ -1,2 +1,18 @@
-package org.online.springocms.models;public class User {
+package org.online.springocms.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    private String FirstName;
+    private String LastName;
+    private String email;
+    private String password;
+    private String role;
 }
