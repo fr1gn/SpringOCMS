642a1f0dbe9fe90e36cc00f3348f8af12b820899
Tairlan <231151@astanait.edu.kz>
2024-02-21 08:37:42 +0600

    OCMS

---
+++ src/main/java/org/online/springocms/SpringOcmsApplication.java
@@ -0,0 +1,13 @@
+package org.online.springocms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOcmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/online/springocms/SpringOcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.online.springocms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOcmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
